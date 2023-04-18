afde2c167f57314d0f9280ffa58db84199704df0
Justezeidotaite <zeidotaite@gmail.com>
2023-04-18 21:53:45 +0300

    second commit

---
+++ script2.js
@@ -0,0 +1,57 @@
+const itemId = localStorage.getItem("id");
+
+const addItemsToScreen = (data) => {
+
+    
+
+    const title = document.getElementById("title")
+    title.innerHTML = data.name
+
+    const price = document.getElementById("price")
+    price.innerHTML = `Price: ${data.price} eur`
+
+    const description = document.getElementById("description")
+    description.innerHTML = data.description
+
+    const location = document.getElementById("location")
+    location.innerHTML = `Location: ${data.location}` 
+
+   const itemPhoto = document.getElementById("image");
+    itemPhoto.setAttribute("style", `background-image: url(${data.photo_url})`);
+
+};
+
+
+fetch(`https://642c60f2d7081590f938f986.mockapi.io/Mugs/${itemId}`)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    addItemsToScreen(data);
+  });
+
+ const goBack = document.querySelector('button')
+
+ goBack.addEventListener("click", () => {
+  window.location.href = "index.html"
+ })
+
+
+const deleteItem = () => {
+  fetch(`https://642c60f2d7081590f938f986.mockapi.io/Mugs/${itemId}`, {
+    method: "DELETE",
+  })
+    .then((res) => {
+  
+        const successMessage = document.getElementById("success_message");
+        successMessage.innerHTML = "Item deleted";
+
+    });
+
+  }
+
+  const trashBin = document.getElementById("trash-button");
+
+    trashBin.addEventListener("click", () => {
+      deleteItem();
+      });
---
+++ script3.js
@@ -0,0 +1,50 @@
+const button = document.getElementById("addItem")
+
+const addNewItem = (newItem) => {
+
+    fetch(`https://642c60f2d7081590f938f986.mockapi.io/Mugs`, { 
+     method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newItem)
+  })
+
+   .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      const successMessage = document.getElementById("success-message");
+      successMessage.innerHTML = "New item added";
+
+      setTimeout(() => {
+        window.location.replace("./index.html");
+      }, 1000);
+    });
+};
+
+
+button.addEventListener("click", () => {
+    const name = document.getElementById("name").value
+    const price = document.getElementById("price").value
+    const description = document.getElementById("description").value
+    const photo_url = document.getElementById("photo_url").value
+    const location = document.getElementById("location").value
+
+    const newItem = {
+        name: name,
+        price: price,
+        description: description,
+        photo_url: photo_url,
+        location: location
+    };
+
+    addNewItem(newItem);
+});
+
+const goBack = document.getElementById('go-back-button')
+
+ goBack.addEventListener("click", () => {
+  window.location.href = "index.html"
+ })
